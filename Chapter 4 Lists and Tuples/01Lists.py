27af9cc26fe944842831691fa0612056ec7b7a18
GamerzUnite <spdpvd3515@gmail.com>
2024-01-30 18:10:05 +0530

    Minor changes

--- Chapter 4 Lists and Tuples/01Lists.py
+++ Chapter 4 Lists and Tuples/01Lists.py
@@ -3,12 +3,14 @@
 #a = ["soham",26,"Mahavir"]
 #a = [24,26,1,3,80]
 a = ["soham","college","Mahavir","friends","studies","etc"] 
+b = []
 # Dont forget to give quotes if its words or string type
 # all The above Examples are of list 
 print(type(a))# this will show new type <class list>
 
 # To access element of the list 
 print(a[1])# Normal List display
+print(b)# we can also create empty list
 'print(a[-1])'
 #Negative index also works in this
 # the Element access system also works on index
--- Chapter 4 Lists and Tuples/02List_Sclicing.py
+++ Chapter 4 Lists and Tuples/02List_Sclicing.py
@@ -1,7 +1,7 @@
 a = ["soham","college","Mahavir","friends","studies","etc"] 
 #\n does not works in strings??!! not conformed
 #Using String sclicing in list sclicing
-print(a[0:4])
+print(a[0:4])# The numbers indicate the range start:end , from where:to where
 print(a[:5])
 print(a[2:-2])
 print(a[:])
