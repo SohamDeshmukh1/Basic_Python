142ecde08cc969bbf5876d8fca39b40ed63edfb2
GamerzUnite <spdpvd3515@gmail.com>
2024-03-03 08:33:56 +0530

    New chapter

---
+++ Chapter 5 Dictionay & Sets/01Dict.py
@@ -0,0 +1,9 @@
+# Syntax : dict_name = {key_name:Key_value}
+# Example :
+
+soham = {
+    'name':'Soham Pravin Deshmukh',
+    "adress":'Home',
+    "education":"Engineering In computer"
+}
+print (soham['name'])
---
+++ Chapter 5 Dictionay & Sets/01Disct_methods.py
@@ -0,0 +1,7 @@
+soham = {
+    'name':'Soham Pravin Deshmukh',
+    "adress":'Home',
+    "education":"Engineering In computer"
+}
+print (soham['name'])
+
