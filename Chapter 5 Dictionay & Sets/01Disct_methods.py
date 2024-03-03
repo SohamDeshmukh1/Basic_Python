f4f474b280eef36e4dc6be10277d07964af1dca3
GamerzUnite <spdpvd3515@gmail.com>
2024-03-03 08:57:50 +0530

    Methods of dict

--- Chapter 5 Dictionay & Sets/01Disct_methods.py
+++ Chapter 5 Dictionay & Sets/01Disct_methods.py
@@ -5,3 +5,7 @@ soham = {
 }
 print (soham['name'])
 
+# Methods
+print(list(soham.keys())) #Shows keys of the dict
+print(soham.values()) #Shows values of keys
+print(soham.items()) #Shows all value of the dict
